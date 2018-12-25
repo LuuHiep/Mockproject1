81ba3615e76f65e6ceb3bef9d28a4a113c089530
Vitcon <viet.tranquoc.96@gmail.com>
2018-12-25 17:23:49 +0700

    create tab

---
+++ app/src/main/java/app/meetup/view/fragment/MyPageFragment.java
@@ -0,0 +1,44 @@
+package app.meetup.view.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import app.meetup.R;
+import app.meetup.presenter.BasePresenter;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MyPageFragment extends BaseFragment {
+
+
+    public MyPageFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.fragment_my_page;
+    }
+
+    @Override
+    protected BasePresenter initPresenter() {
+        return null;
+    }
+
+    @Override
+    protected void initData(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState, View rootView) {
+
+    }
+
+}
